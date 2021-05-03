c2cc06b63c4ebef322fcc0ae2bf7bd83243fb71c
Juan P Murdolo <jpmurdolo@gmail.com>
2021-05-02 22:27:38 -0300

    se agregaron POJO's

---
+++ sfg-pet-clinic/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Mascota.java
@@ -0,0 +1,34 @@
+package globallogic.juanmurdolo.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Mascota {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate fechaNacimiento;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
