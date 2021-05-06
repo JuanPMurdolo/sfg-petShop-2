caafc6819c13627fd1f6293cc6a4af404d72363f
Juan P Murdolo <jpmurdolo@gmail.com>
2021-05-06 11:00:30 -0300

    Listado de veterinarios

--- sfg-pet-clinic/pet-clinic-web/src/main/java/globallogic/juanmurdolo/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/globallogic/juanmurdolo/sfgpetclinic/controllers/VetController.java
@@ -1,15 +1,27 @@
 package globallogic.juanmurdolo.sfgpetclinic.controllers;
 
+
+import globallogic.juanmurdolo.sfgpetclinic.servicios.VeterinarioService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VeterinarioService veterinarioService;
+
+    public VetController(VeterinarioService veterinarioService) {
+        this.veterinarioService = veterinarioService;
+    }
+
 
     @RequestMapping({"", "/index",  "/index.html"})
-    public String listVets(){
+    public String listVets(Model model)
+    {
+        model.addAttribute("veterinarios", veterinarioService.findAll());
+
         return "vets/index";
     }
 }
