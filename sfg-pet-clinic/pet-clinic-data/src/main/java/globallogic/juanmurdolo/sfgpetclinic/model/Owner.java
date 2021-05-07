bfcb6dd7dcb2c5ed96bd2e37f9b172b82ec03299
Juan P Murdolo <jpmurdolo@gmail.com>
2021-05-07 16:12:41 -0300

    Servicio de PetType

--- sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Owner.java
@@ -5,6 +5,34 @@ import java.util.Set;
 public class Owner extends Person{
 
     private Set<Mascota> mascotas;
+    private String direccion;
+    private String ciudad;
+    private String telefono;
+
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 
     public Set<Mascota> getMascotas() {
         return mascotas;
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/servicios/PetTypeService.java
@@ -0,0 +1,8 @@
+package globallogic.juanmurdolo.sfgpetclinic.servicios;
+
+import globallogic.juanmurdolo.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+
+}
