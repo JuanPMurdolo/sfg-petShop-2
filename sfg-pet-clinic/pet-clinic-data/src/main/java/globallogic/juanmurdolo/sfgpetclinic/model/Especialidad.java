124febb4429d4d839f42cac160c3014f287d148f
Juan P Murdolo <jpmurdolo@gmail.com>
2021-05-07 15:41:48 -0300

    Creacion de especialidad y relacion con veterinario

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Especialidad.java
@@ -0,0 +1,14 @@
+package globallogic.juanmurdolo.sfgpetclinic.model;
+
+public class Especialidad extends BaseEntity{
+
+    private String descripcion;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
--- sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Veterinario.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Veterinario.java
@@ -1,6 +1,16 @@
 package globallogic.juanmurdolo.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Veterinario extends Person{
 
+    private Set<Especialidad> especialidad;
 
+    public Set<Especialidad> getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Set<Especialidad> especialidad) {
+        this.especialidad = especialidad;
+    }
 }
