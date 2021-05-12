4847eab4c14ed601767adf050a6d57fa9b3f04e0
Juan P Murdolo <jpmurdolo@gmail.com>
2021-05-12 15:16:45 -0300

    148 Veterinario y Especialidades JPA

--- sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Especialidad.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Especialidad.java
@@ -1,8 +1,17 @@
 package globallogic.juanmurdolo.sfgpetclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "especialidad")
 public class Especialidad extends BaseEntity{
 
+    @Column(name = "descripcion")
     private String descripcion;
+
 
     public String getDescripcion() {
         return descripcion;
--- sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Veterinario.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/globallogic/juanmurdolo/sfgpetclinic/model/Veterinario.java
@@ -1,10 +1,17 @@
 package globallogic.juanmurdolo.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Entity
+@Table(name = "veterinarios")
 public class Veterinario extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable (name = "vet_especialidades", joinColumns = @JoinColumn(name = "veterinario_id"),
+            inverseJoinColumns = @JoinColumn(name = "especialidad_id"))
     private Set<Especialidad> especialidad = new HashSet<>();
 
     public Set<Especialidad> getEspecialidad() {
