1b1df6476a3af0bc77d399fef0c71b14b1faf69b
Juan P Murdolo <jpmurdolo@gmail.com>
2021-06-01 12:56:13 -0300

    246 Retornar los veterinarios como objetos JSON

--- sfg-pet-clinic/pet-clinic-web/src/main/java/globallogic/juanmurdolo/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/globallogic/juanmurdolo/sfgpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package globallogic.juanmurdolo.sfgpetclinic.controllers;
 
 
+import globallogic.juanmurdolo.sfgpetclinic.model.Veterinario;
 import globallogic.juanmurdolo.sfgpetclinic.services.VeterinarioService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,5 +26,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Veterinario> getVetsJson(){
+        return vetService.findAll();
     }
 }
